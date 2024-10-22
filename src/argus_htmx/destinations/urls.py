836cc7028fe9b3dd711297ac54857a56123d797b
Simon Oliver Tveit <simtve@uninett.no>
2024-10-22 14:21:44 +0200

    Use destinations view instead of placeholder

--- src/argus_htmx/destinations/urls.py
+++ src/argus_htmx/destinations/urls.py
@@ -1,21 +1,8 @@
-from django.http import HttpResponse
-from django.template import Template, RequestContext
 from django.urls import path
 
+from . import views
 
-def placeholder(request):
-    template = Template(
-        """{% extends "htmx/base.html" %}
-        {% block main %}
-        <h1>DESTINATION PLACEHOLDER</h1>
-        {% endblock main %}
-        """
-    )
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
-
-
-app_name = "htmx"
+app_name = "destinations"
 urlpatterns = [
-    path("", placeholder, name="destination-placeholder"),
+    path("", views.destinations, name="destinations"),
 ]
