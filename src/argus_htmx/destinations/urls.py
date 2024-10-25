b60fb714e0c918932e2fe931380ab3dff658ba40
Simon Oliver Tveit <simtve@uninett.no>
2024-10-25 15:22:44 +0200

    Change endpoint for updating destination
    from ../update_destionation/<pk> to  ../<pk>

--- src/argus_htmx/destinations/urls.py
+++ src/argus_htmx/destinations/urls.py
@@ -6,6 +6,6 @@ app_name = "htmx"
 urlpatterns = [
     path("", views.destinations, name="destinations"),
     path("create_destination/", views.destinations_create, name="destinations-create"),
-    path("update_destination/<int:pk>/", views.destinations_update, name="destinations-update"),
+    path("<int:pk>/", views.destinations_update, name="destinations-update"),
     path("delete_destination/<int:pk>/", views.destinations_delete, name="destinations-delete"),
 ]
