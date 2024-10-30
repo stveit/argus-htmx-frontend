21a8bc7a37edf9050a27ec1d1b40d75dc2dd6974
Simon Oliver Tveit <simtve@uninett.no>
2024-10-30 16:13:57 +0100

    Add view for deleting a destination
    Pretty much copied from the destinations API, needs some changes
    so it wont just crash to 500 when it raises a ValidationError

--- src/argus_htmx/destinations/views.py
+++ src/argus_htmx/destinations/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse
+
+from rest_framework.exceptions import ValidationError
+
+from argus.notificationprofile.media import api_safely_get_medium_object
+from argus.notificationprofile.media.base import NotificationMedium
 
 from .forms import DestinationFormCreate
 
@@ -19,6 +24,21 @@ def destinations_create(request) -> HttpResponse:
     if form.is_valid():
         form.save()
     return _render_destinations(request)
+
+
+@require_http_methods(["POST"])
+def destinations_delete(request, pk: int) -> HttpResponse:
+    destination = get_object_or_404(request.user.destinations.all(), pk=pk)
+
+    try:
+        medium = api_safely_get_medium_object(destination.media.slug)
+        medium.raise_if_not_deletable(destination)
+    except NotificationMedium.NotDeletableError as e:
+        raise ValidationError(str(e))
+    else:
+        destination.delete()
+        return _render_destinations(request)
+
 
 def _render_destinations(request) -> HttpResponse:
     return render(request, "htmx/destinations/destinations.html")
