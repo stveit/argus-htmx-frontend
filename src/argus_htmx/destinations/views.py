fc5d501c63574a53396520bcef97605b881abe9f
Simon Oliver Tveit <simtve@uninett.no>
2024-10-30 14:11:21 +0100

    Add view for destinations page

---
+++ src/argus_htmx/destinations/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse
+
+
+@require_http_methods(["GET"])
+def destinations(request) -> HttpResponse:
+    return render(request, "htmx/destinations/destinations.html")
