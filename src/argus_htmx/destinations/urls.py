b874793f4a4ac38ef2944ddabf597fb213e269fa
Simon Oliver Tveit <simtve@uninett.no>
2024-10-30 16:13:57 +0100

    Add view for deleting a destination
    Pretty much copied from the destinations API, needs some changes
    so it wont just crash to 500 when it raises a ValidationError

--- src/argus_htmx/destinations/urls.py
+++ src/argus_htmx/destinations/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import destinations
+from .views import destinations, destinations_delete
 
 app_name = "htmx"
 urlpatterns = [
     path("", destinations, name="destinations"),
+    path("<int:pk>/delete", destinations_delete, name="destinations-delete"),
 ]
