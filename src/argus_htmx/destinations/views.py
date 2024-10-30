ae622b1be327d6ea1a791247c270741afa3e9c90
Simon Oliver Tveit <simtve@uninett.no>
2024-10-30 16:08:10 +0100

    Add view for creating a destination

--- src/argus_htmx/destinations/views.py
+++ src/argus_htmx/destinations/views.py
@@ -3,7 +3,19 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse
 
+from .forms import DestinationFormCreate
 
-@require_http_methods(["GET"])
-def destinations(request) -> HttpResponse:
+
+@require_http_methods(["GET", "POST"])
+def destinations(request):
+    if request.method == "GET":
+        return render(request, "htmx/destinations/destinations.html")
+    elif request.method == "POST":
+        return destinations_create(request)
+
+
+def destinations_create(request) -> HttpResponse:
+    form = DestinationFormCreate(request.POST or None, request=request)
+    if form.is_valid():
+        form.save()
     return render(request, "htmx/destinations/destinations.html")
